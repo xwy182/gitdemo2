8949c4e514b6f5cd50a2fb2d255453913d0c75bb
xwy_glb <xwy_glb@qq.com>
2020-11-25 11:09:13 +0800

    add添加代码

---
+++ src/main/java/com/kgc/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.kgc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/show1.do")
+    public String test(Map<String,Object>map){
+        map.put("mm","测试传值");
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 86182
+  Date: 2020/11/17
+  Time: 14:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>测试</h1>
+<p>${mm}</p>
+</body>
+</html>
