2fce15cf813d52acacd6a458fc91c65233a968dc
xwy_glb <xwy_glb@qq.com>
2022-04-29 16:00:32 +0800

    222ce1

--- src/main/java/com/kgc/controller/IndexController.java
+++ src/main/java/com/kgc/controller/IndexController.java
@@ -11,6 +11,8 @@ public class IndexController {
     @RequestMapping("/show1.do")
     public String test(Map<String,Object>map){
         map.put("mm","测试传值q1111");
+        map.put("mm","测试传值22222");
+        map.put("mm","测试传值1111");
         return "index";
     }
 }
